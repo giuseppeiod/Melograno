02f6148563642ecd0f79b16a8d7a4b1598a80883
Giuseppe <giodice22@fed.idserve.net>
2023-05-10 09:49:05 +0200

    Nuovo Progetto

---
+++ Melograno/MelogranoProject/MelogranoProjectApp.swift
@@ -0,0 +1,20 @@
+//
+//  MelogranoProjectApp.swift
+//  MelogranoProject
+//
+//  Created by Giuseppe Iodice on 10/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MelogranoProjectApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
