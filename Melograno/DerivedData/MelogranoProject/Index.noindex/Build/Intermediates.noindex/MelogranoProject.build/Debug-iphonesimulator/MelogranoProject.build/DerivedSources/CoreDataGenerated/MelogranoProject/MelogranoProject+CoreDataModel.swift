05de2ca5f51d7d55735c2e6021646680a972208e
brillantina <rita.marrano.98@gmail.com>
2023-05-16 12:26:45 +0200

    prova

---
+++ Melograno/DerivedData/MelogranoProject/Index.noindex/Build/Intermediates.noindex/MelogranoProject.build/Debug-iphonesimulator/MelogranoProject.build/DerivedSources/CoreDataGenerated/MelogranoProject/MelogranoProject+CoreDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  MelogranoProject+CoreDataModel.swift
+//  
+//
+//  Created by Rita Marrano on 16/05/23.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+
---
+++ Melograno/DerivedData/MelogranoProject/Index.noindex/Build/Intermediates.noindex/MelogranoProject.build/Debug-iphonesimulator/MelogranoProject.build/DerivedSources/CoreDataGenerated/MelogranoProject/season+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  season+CoreDataClass.swift
+//  
+//
+//  Created by Rita Marrano on 16/05/23.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(season)
+public class season: NSManagedObject {
+
+}
