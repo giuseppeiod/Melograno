c66f6ad8bd33a5e7a35bd57ba765859d32527815
Giuseppe <giodice22@fed.idserve.net>
2023-05-16 12:22:39 +0200

    Tasto Back gioco3 che funziona

--- Melograno/MelogranoProject/CardMemoryGameView.swift
+++ Melograno/MelogranoProject/CardMemoryGameView.swift
@@ -32,79 +32,87 @@ struct CardsMemoryGameView: View {
     }
     var body: some View {
         
-        ZStack{
-            VStack(alignment: .center){
-                
-                Text("Match the cards")
-                    .font(.largeTitle)
-                
-                
-                NavBar()
-                
-                LazyVGrid(columns: col, spacing: 3){
-                    ForEach(game.cards){ card in
-                        
-                        CardView(card: card, onCardTap: {
-                            
-                            game.choose(card)
-                            
-                        })
-                        
-                        
-                        
-                    }
-                }
-                
-                if game.isGameFinished {
-                    Text("Congratulations! You matched all the cards!")
-                        .font(.headline)
-                        .foregroundColor(.green)
+        
+        if isGameFinishedButton{
+            ContentView()
+        }else{
+            ZStack{
+                VStack(alignment: .center){
                     
-                    Button(action: {
-                        restartGame()
-                    }, label: {
-                        Text("Restart Game")
+                    Text("Match the cards")
+                        .font(.largeTitle)
+                    
+                    
+                    NavBar()
+                    
+                    LazyVGrid(columns: col, spacing: 3){
+                        ForEach(game.cards){ card in
+                            
+                            CardView(card: card, onCardTap: {
+                                
+                                game.choose(card)
+                                
+                            })
+                            
+                            
+                            
+                        }
+                    }
+                    
+                    if game.isGameFinished {
+                        Text("Congratulations! You matched all the cards!")
                             .font(.headline)
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(Color.blue)
-                            .cornerRadius(10)
-                    })
+                            .foregroundColor(.green)
+                        
+                        Button(action: {
+                            restartGame()
+                        }, label: {
+                            Text("Restart Game")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.blue)
+                                .cornerRadius(10)
+                        })
+                    }
+                    
                 }
-
+                
+                //            if game.isGameFinished {
+                //                Text("Congratulations! You matched all the cards!")
+                //                    .font(.headline)
+                //                    .foregroundColor(.green)
+                //
+                //                Button(action: {
+                //                    restartGame()
+                //                }, label: {
+                //                    Text("Restart Game")
+                //                        .font(.headline)
+                //                        .foregroundColor(.white)
+                //                        .padding()
+                //                        .background(Color.blue)
+                //                        .cornerRadius(10)
+                //                })
+                //            }
+                
             }
-            
-//            if game.isGameFinished {
-//                Text("Congratulations! You matched all the cards!")
-//                    .font(.headline)
-//                    .foregroundColor(.green)
-//
-//                Button(action: {
-//                    restartGame()
-//                }, label: {
-//                    Text("Restart Game")
-//                        .font(.headline)
-//                        .foregroundColor(.white)
-//                        .padding()
-//                        .background(Color.blue)
-//                        .cornerRadius(10)
-//                })
-//            }
-            
         }
+        
     }
-    
-    
     
     
     @ViewBuilder
     func NavBar() -> some View {
+        
+        
         HStack(spacing: 18) {
             Button(action: {
  
                 isGameFinishedButton = true
                 
                 print("Button pressed")
+                
+   
             }) {
                 Image(systemName: "xmark")
                     .font(.title)
@@ -146,5 +154,6 @@ struct CardsMemoryGameView_Previews: PreviewProvider {
         CardsMemoryGameView()
     }
 }
+
 
 
