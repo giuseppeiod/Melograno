11cc6289480ca87beab2b81e0f54047c4fc9c036
Giuseppe <giodice22@fed.idserve.net>
2023-05-23 10:21:07 +0200

    Modifica game 1, adesso i file json vengono caricati randomicamente

--- Melograno/MelogranoProject/Model/ViewModelGame1.swift
+++ Melograno/MelogranoProject/Model/ViewModelGame1.swift
@@ -6,7 +6,108 @@
 //
 
 
+
 import SwiftUI
+
+class ViewModelGame1: ObservableObject {
+    @Published var cards: [CardGame1] = []
+    @Published var cardsToOrder: [CardGame1] = []
+    @Published var selectedCardIndex: Int?
+    @Published var selectedCardToOrderIndex: Int?
+
+    @Published var hiddenCardIDs: Set<Int> = []
+
+    init() {
+        loadJson()
+            
+    }
+
+    func loadJson() {
+        if let url = Bundle.main.url(forResource: "Game1", withExtension: "geojson") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                let jsonData = try decoder.decode([[CardGame1]].self, from: data)
+
+                // Seleziona un array di carte in modo casuale dal JSON
+                let randomCardsData = jsonData.randomElement() ?? []
+
+                // Converti i dati decodificati in CardGame1
+                self.cards = randomCardsData.map { CardGame1(id: $0.id, correctIndex: $0.correctIndex, imageName: $0.imageName) }
+                
+                // Crea cardsToOrder con gli stessi elementi di cards ma con imageName vuoto e id differente
+                self.cardsToOrder = self.cards.enumerated().map {
+                    CardGame1(id: $0.offset + self.cards.count + 1, correctIndex: $0.element.correctIndex, imageName: "")
+                }
+
+                // Aggiungi un po' di logging
+                print("Caricamento dati riuscito!")
+                print("cards: \(self.cards)")
+                print("cardsToOrder: \(self.cardsToOrder)")
+
+            } catch {
+                print("Errore durante la lettura del file JSON: \(error)")
+            }
+        } else {
+            print("Impossibile trovare il file JSON.")
+        }
+    }
+
+
+
+
+
+    var allCardsPlaced: Bool {
+        return cardsToOrder.allSatisfy { $0.imageName != "" }
+    }
+
+    func resetCardToOrderImage(at index: Int) {
+        guard index >= 0 && index < cardsToOrder.count else {
+            return
+        }
+
+        let imageName = cardsToOrder[index].imageName
+
+        guard let originalCardIndex = cards.firstIndex(where: { $0.imageName == imageName }) else {
+            return
+        }
+
+        let cardID = cards[originalCardIndex].id
+
+        if hiddenCardIDs.contains(cardID) {
+            hiddenCardIDs.remove(cardID)
+        }
+        else {
+            hiddenCardIDs.insert(cardID)
+        }
+
+        if cardsToOrder[index].imageName != "" {
+            cardsToOrder[index].imageName = ""
+        }
+    }
+
+    func replaceCardImage() {
+        guard let selectedIndex = selectedCardIndex,
+              let selectedToOrderIndex = selectedCardToOrderIndex,
+              selectedIndex >= 0 && selectedIndex < cards.count,
+              selectedToOrderIndex >= 0 && selectedToOrderIndex < cardsToOrder.count
+        else {
+            return
+        }
+
+        let selectedCard = cards[selectedIndex]
+        var updateCards = cards
+        updateCards[selectedIndex].isHidden.toggle()
+
+        var updatedCardsToOrder = cardsToOrder
+        updatedCardsToOrder[selectedToOrderIndex].imageName = selectedCard.imageName
+
+        cardsToOrder = updatedCardsToOrder
+
+        selectedCardIndex = nil
+        selectedCardToOrderIndex = nil
+    }
+}
 
 /*class ViewModelGame1: ObservableObject{
     
@@ -111,101 +212,3 @@ import SwiftUI
 }
 /*
  */*/
-import SwiftUI
-
-class ViewModelGame1: ObservableObject {
-    @Published var cards: [CardGame1] = []
-    @Published var cardsToOrder: [CardGame1] = []
-    @Published var selectedCardIndex: Int?
-    @Published var selectedCardToOrderIndex: Int?
-
-    @Published var hiddenCardIDs: Set<Int> = []
-
-    init() {
-        loadJson()
-    }
-
-    func loadJson() {
-        if let url = Bundle.main.url(forResource: "Game2", withExtension: "geojson") {
-            do {
-                let data = try Data(contentsOf: url)
-                let decoder = JSONDecoder()
-                let jsonData = try decoder.decode([[CardGame1]].self, from: data)
-
-                // Converti i dati decodificati in CardGame1
-                self.cards = jsonData[0].map { CardGame1(id: $0.id, correctIndex: $0.correctIndex, imageName: $0.imageName) }
-                
-                // Crea cardsToOrder con gli stessi elementi di cards ma con imageName vuoto e id differente
-                self.cardsToOrder = self.cards.enumerated().map {
-                    CardGame1(id: $0.offset + self.cards.count , correctIndex: $0.element.correctIndex, imageName: "")
-                }
-
-                // Aggiungi un po' di logging
-                print("Caricamento dati riuscito!")
-                print("cards: \(self.cards)")
-                print("cardsToOrder: \(self.cardsToOrder)")
-
-            } catch {
-                print("Errore durante la lettura del file JSON: \(error)")
-            }
-        } else {
-            print("Impossibile trovare il file JSON.")
-        }
-    }
-
-
-
-
-
-
-    var allCardsPlaced: Bool {
-        return cardsToOrder.allSatisfy { $0.imageName != "" }
-    }
-
-    func resetCardToOrderImage(at index: Int) {
-        guard index >= 0 && index < cardsToOrder.count else {
-            return
-        }
-
-        let imageName = cardsToOrder[index].imageName
-
-        guard let originalCardIndex = cards.firstIndex(where: { $0.imageName == imageName }) else {
-            return
-        }
-
-        let cardID = cards[originalCardIndex].id
-
-        if hiddenCardIDs.contains(cardID) {
-            hiddenCardIDs.remove(cardID)
-        }
-        else {
-            hiddenCardIDs.insert(cardID)
-        }
-
-        if cardsToOrder[index].imageName != "" {
-            cardsToOrder[index].imageName = ""
-        }
-    }
-
-    func replaceCardImage() {
-        guard let selectedIndex = selectedCardIndex,
-              let selectedToOrderIndex = selectedCardToOrderIndex,
-              selectedIndex >= 0 && selectedIndex < cards.count,
-              selectedToOrderIndex >= 0 && selectedToOrderIndex < cardsToOrder.count
-        else {
-            return
-        }
-
-        let selectedCard = cards[selectedIndex]
-        var updateCards = cards
-        updateCards[selectedIndex].isHidden.toggle()
-
-        var updatedCardsToOrder = cardsToOrder
-        updatedCardsToOrder[selectedToOrderIndex].imageName = selectedCard.imageName
-
-        cardsToOrder = updatedCardsToOrder
-
-        selectedCardIndex = nil
-        selectedCardToOrderIndex = nil
-    }
-}
