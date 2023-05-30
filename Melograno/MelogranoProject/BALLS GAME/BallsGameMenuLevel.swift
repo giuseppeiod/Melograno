12f385531d19630178bda9bd3732be841994387a
marcosantopietro <marcosantopietrowar@gmail.com>
2023-05-30 12:35:32 +0200

    suono menu difficoltà gioco palle
    burdell ma ce l'ho fatta

--- Melograno/MelogranoProject/BALLS GAME/BallsGameMenuLevel.swift
+++ Melograno/MelogranoProject/BALLS GAME/BallsGameMenuLevel.swift
@@ -1,22 +1,266 @@
+////
+////  BallsGameMenuLevel.swift
+////  MelogranoProject
+////
+////  Created by Rita Marrano on 23/05/23.
+////
 //
-//  BallsGameMenuLevel.swift
-//  MelogranoProject
+//import SwiftUI
 //
-//  Created by Rita Marrano on 23/05/23.
+//import AVFoundation
 //
+//class BallsGameLevelSelection: ObservableObject {
+//    @Published var selectedGame: String?
+//    @State private var audioPlayer: AVAudioPlayer?
+//}
+//
+//struct BallsGameMenuLevel: View {
+//    @StateObject var gameSelection = BallsGameLevelSelection()
+//
+//    var body: some View {
+//        switch gameSelection.selectedGame {
+//        case "Level 1":
+//            ContentViewBallsLevel1()
+//        case "Level 2":
+//            ContentViewBallsLevel2()
+//        case "Level 3":
+//            ContentViewBallsLevel3()
+//        default:
+//            BallsGameMenuLevelModel(gameSelection: gameSelection)
+//        }
+//    }
+//}
+//
+//struct BallsGameMenuLevelModel: View {
+//    @ObservedObject var gameSelection: BallsGameLevelSelection
+//
+//    @State private var turnBack = false
+//
+//    let games = ["Level 1", "Level 2", "Level 3"]
+//    var buttonImagesGame: [String] = ["level1", "level2", "level3", "level4"]
+//    var body: some View {
+//
+//
+//        if turnBack{
+//            ContentView1()
+//        }
+//        else{
+//            VStack {
+//
+//
+//                HStack{
+//
+//
+//                    Button(action: {
+//
+//                        turnBack = true
+//
+//                        print("Button pressed")
+//
+//
+//                    }) {
+//                        Image(systemName: "chevron.left")
+//                            .font(.title)
+//                            .foregroundColor(.gray)
+//                        Text("Back")
+//                            .font(.headline)
+//                            .foregroundColor(.gray)
+//                    }
+//
+//                    Spacer()
+//
+//                }.padding(30)
+//
+//
+//
+//
+//
+//
+//                Text("BALLSATHLON")
+//
+//                    .font(.system(size: 80))
+//                    .padding()
+//
+//                Text("The balls will ligh up, remember the longest sequence of colors possible!")
+//                    .font(.largeTitle)
+//                    .padding()
+//
+//
+//                Spacer()
+//
+//
+//
+//                HStack(spacing: 90){
+//                    ForEach(games.indices, id: \.self) { game in
+//                    Button(action: {
+//                        self.gameSelection.selectedGame = games[game]
+//                    }) {
+//                        Image(buttonImagesGame[game])
+//
+////
+////                                                Text("\(game)")
+////                                                    .font(.title)
+////                                                    .padding()
+////                                                    .frame(width: 140, height: 140)
+////                                                    .background(Color.blue)
+////                                                    .foregroundColor(.white)
+////                                                    .cornerRadius(10)
+//                                            }
+//                                            .padding(.bottom, 10)
+//
+//
+//                    }
+//                }
+//                Spacer()
+//            }
+//
+//        }
+//    }
+//}
+//
+//
+//
+//
+//struct BallsGameMenuLevel_Previews: PreviewProvider {
+//    static var previews: some View {
+//        BallsGameMenuLevel()
+//    }
+//}
+
+//import SwiftUI
+//import AVFoundation
+//
+//class BallsGameLevelSelection: ObservableObject {
+//    @Published var selectedGame: String?
+//    @State private var audioPlayer: AVAudioPlayer?
+//}
+//
+//struct BallsGameMenuLevel: View {
+//    @StateObject var gameSelection = BallsGameLevelSelection()
+//
+//    var body: some View {
+//        switch gameSelection.selectedGame {
+//        case "Level 1":
+//            ContentViewBallsLevel1()
+//        case "Level 2":
+//            ContentViewBallsLevel2()
+//        case "Level 3":
+//            ContentViewBallsLevel3()
+//        default:
+//            BallsGameMenuLevelModel(gameSelection: gameSelection)
+//        }
+//    }
+//}
+//
+//struct BallsGameMenuLevelModel: View {
+//    @ObservedObject var gameSelection: BallsGameLevelSelection
+//    @State private var turnBack = false
+//    let games = ["Level 1", "Level 2", "Level 3"]
+//    var buttonImagesGame: [String] = ["level1", "level2", "level3", "level4"]
+//
+//    var body: some View {
+//        if turnBack {
+//            ContentView1()
+//        } else {
+//            VStack {
+//                HStack {
+//                    Button(action: {
+//                        turnBack = true
+//                        print("Button pressed")
+//                    }) {
+//                        Image(systemName: "chevron.left")
+//                            .font(.title)
+//                            .foregroundColor(.gray)
+//                        Text("Back")
+//                            .font(.headline)
+//                            .foregroundColor(.gray)
+//                    }
+//                    Spacer()
+//                }
+//                .padding(30)
+//
+//                Text("BALLSATHLON")
+//                    .font(.system(size: 80))
+//                    .padding()
+//
+//                Text("The balls will light up, remember the longest sequence of colors possible!")
+//                    .font(.largeTitle)
+//                    .padding()
+//
+//                Spacer()
+//
+//                HStack(spacing: 90) {
+//                    ForEach(games.indices, id: \.self) { game in
+//                        Button(action: {
+//                            self.gameSelection.selectedGame = games[game]
+//                            DispatchQueue.main.async {
+//                                // Codice di riproduzione audio
+//                                playTapSound()
+//                            }
+// // Aggiungi questa linea per riprodurre il feedback sonoro
+//                        }) {
+//                            Image(buttonImagesGame[game])
+//                        }
+//                        .padding(.bottom, 10)
+//                    }
+//                }
+//
+//                Spacer()
+//            }
+//        }
+//    }
+//
+//    func playTapSound() {
+//        do {
+//            if let soundURL = Bundle.main.url(forResource: "menu", withExtension: "mp3") {
+//                let audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+//                audioPlayer.play()
+//            }
+//        } catch {
+//            print("Failed to load sound file")
+//        }
+//    }
+//}
+//
+//struct BallsGameMenuLevel_Previews: PreviewProvider {
+//    static var previews: some View {
+//        BallsGameMenuLevel()
+//    }
+//}
 
 import SwiftUI
-
 import AVFoundation
 
 class BallsGameLevelSelection: ObservableObject {
     @Published var selectedGame: String?
-    @State private var audioPlayer: AVAudioPlayer?
+    private var audioPlayer: AVAudioPlayer?
+    
+    init() {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback)
+        } catch {
+            print("Failed to set audio session category: \(error)")
+        }
+    }
+    
+    func playTapSound() {
+        guard let soundURL = Bundle.main.url(forResource: "menu", withExtension: "mp3") else {
+            print("Failed to find tap sound file")
+            return
+        }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+            audioPlayer?.play()
+        } catch {
+            print("Failed to play tap sound: \(error)")
+        }
+    }
 }
 
 struct BallsGameMenuLevel: View {
     @StateObject var gameSelection = BallsGameLevelSelection()
-
+    
     var body: some View {
         switch gameSelection.selectedGame {
         case "Level 1":
@@ -38,26 +282,16 @@ struct BallsGameMenuLevelModel: View {
     
     let games = ["Level 1", "Level 2", "Level 3"]
     var buttonImagesGame: [String] = ["level1", "level2", "level3", "level4"]
+    
     var body: some View {
-        
-        
-        if turnBack{
+        if turnBack {
             ContentView1()
-        }
-        else{
+        } else {
             VStack {
-                
-                
-                HStack{
-                    
-                    
+                HStack {
                     Button(action: {
-                        
                         turnBack = true
-                        
-                        print("Button pressed")
-                        
-                        
+                        print("Back button pressed")
                     }) {
                         Image(systemName: "chevron.left")
                             .font(.title)
@@ -66,58 +300,37 @@ struct BallsGameMenuLevelModel: View {
                             .font(.headline)
                             .foregroundColor(.gray)
                     }
-                    
                     Spacer()
-                    
-                }.padding(30)
-
-                
-                
-                
-                
+                }
+                .padding(30)
                 
                 Text("BALLSATHLON")
-                    
                     .font(.system(size: 80))
                     .padding()
-
-                Text("The balls will ligh up, remember the longest sequence of colors possible!")
+                
+                Text("The balls will light up. Remember the longest sequence of colors possible!")
                     .font(.largeTitle)
                     .padding()
                 
-                
                 Spacer()
                 
-
-                
-                HStack(spacing: 90){
+                HStack(spacing: 90) {
                     ForEach(games.indices, id: \.self) { game in
-                    Button(action: {
-                        self.gameSelection.selectedGame = games[game]
-                    }) {
-                        Image(buttonImagesGame[game])
-                        
-//
-//                                                Text("\(game)")
-//                                                    .font(.title)
-//                                                    .padding()
-//                                                    .frame(width: 140, height: 140)
-//                                                    .background(Color.blue)
-//                                                    .foregroundColor(.white)
-//                                                    .cornerRadius(10)
-                                            }
-                                            .padding(.bottom, 10)
-                        
-
+                        Button(action: {
+                            self.gameSelection.selectedGame = games[game]
+                            self.gameSelection.playTapSound()
+                        }) {
+                            Image(buttonImagesGame[game])
+                        }
+                        .padding(.bottom, 10)
                     }
                 }
+                
                 Spacer()
             }
-            
         }
     }
 }
-
 
 struct BallsGameMenuLevel_Previews: PreviewProvider {
     static var previews: some View {
